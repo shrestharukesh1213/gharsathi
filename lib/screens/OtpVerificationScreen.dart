101b4745587848f58b393635b0bb4a0999559cff
Kshitiz Shrestha <kshitizshrestha@Kshitizs-MacBook-Air.local>
2024-09-17 18:01:42 +0545

    OtpVerificationScreen Creation

---
+++ lib/screens/OtpVerificationScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Otpverificationscreen extends StatefulWidget {
+  const Otpverificationscreen({super.key});
+
+  @override
+  State<Otpverificationscreen> createState() => _OtpverificationscreenState();
+}
+
+class _OtpverificationscreenState extends State<Otpverificationscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
