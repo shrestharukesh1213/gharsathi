fb16a5344401a858c2a0448a7e44691f53ebee73
rohaan011 <rohanpzpt6@gmail.com>
2024-09-21 19:57:46 +0545

    tenant home screen

---
+++ lib/screens/RoomScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Roomscreen extends StatefulWidget {
+  const Roomscreen({super.key});
+
+  @override
+  State<Roomscreen> createState() => _RoomscreenState();
+}
+
+class _RoomscreenState extends State<Roomscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
