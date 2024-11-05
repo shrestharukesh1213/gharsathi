9d384958048897620f477f69ef46e854e9fdd350
Rukesh Shrestha <125758326+shrestharukesh1213@users.noreply.github.com>
2024-11-05 22:02:30 +0545

    Fix pixel overflow

--- lib/screens/TenantHomeScreen.dart
+++ lib/screens/TenantHomeScreen.dart
@@ -106,64 +106,70 @@ class _TenanthomescreenState extends State<Tenanthomescreen> {
               }).toList();
             }),
           ),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                FutureBuilder<QuerySnapshot?>(
-                    future: filterCategory == "all"
-                        ? FirebaseFirestore.instance.collection('rooms').get()
-                        : FirebaseFirestore.instance
-                            .collection('rooms')
-                            .where("category", isEqualTo: filterCategory)
-                            .get(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return const Center(child: Text('Failed to load data'));
-                      } else if (!snapshot.hasData) {
-                        return const Center(child: Text('No data found'));
-                      } else {
-                        final data = snapshot.data!.docs;
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  FutureBuilder<QuerySnapshot?>(
+                      future: filterCategory == "all"
+                          ? FirebaseFirestore.instance.collection('rooms').get()
+                          : FirebaseFirestore.instance
+                              .collection('rooms')
+                              .where("category", isEqualTo: filterCategory)
+                              .get(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else if (snapshot.hasError) {
+                          return const Center(
+                              child: Text('Failed to load data'));
+                        } else if (!snapshot.hasData) {
+                          return const Center(child: Text('No data found'));
+                        } else {
+                          final data = snapshot.data!.docs;
 
-                        return ListView.builder(
-                            // crossAxisCount: 1,
-                            // childAspectRatio: 0.5,
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: data.length,
-                            // children: List.generate(data.length, (index)
-                            itemBuilder: (context, index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.pushNamed(
-                                    context,
-                                    "/details",
-                                    arguments: {
-                                      "roomTitle": data[index]['name'],
-                                      "postedBy": data[index]['postedBy'],
-                                      "location": data[index]['location'],
-                                      "price": data[index]['price'],
-                                      "description": data[index]['description'],
-                                      "images": data[index]['images'],
-                                      "amenities": data[index]['amenities'],
-                                      "roomId": data[index].id,
-                                    },
-                                  );
-                                },
-                                child: Roomcard(
-                                    roomTitle: data[index]['name'],
-                                    postedBy: data[index]['postedBy'],
-                                    location: data[index]['location'],
-                                    price: data[index]['price'],
-                                    description: data[index]['description'],
-                                    image: data[index]['images'][0],
-                                    amenities: data[index]['amenities']),
-                              );
-                            });
-                      }
-                    })
-              ],
+                          return ListView.builder(
+                              // crossAxisCount: 1,
+                              // childAspectRatio: 0.5,
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: data.length,
+                              // children: List.generate(data.length, (index)
+                              itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      "/details",
+                                      arguments: {
+                                        "roomTitle": data[index]['name'],
+                                        "postedBy": data[index]['postedBy'],
+                                        "location": data[index]['location'],
+                                        "price": data[index]['price'],
+                                        "description": data[index]
+                                            ['description'],
+                                        "images": data[index]['images'],
+                                        "amenities": data[index]['amenities'],
+                                        "roomId": data[index].id,
+                                      },
+                                    );
+                                  },
+                                  child: Roomcard(
+                                      roomTitle: data[index]['name'],
+                                      postedBy: data[index]['postedBy'],
+                                      location: data[index]['location'],
+                                      price: data[index]['price'],
+                                      description: data[index]['description'],
+                                      image: data[index]['images'][0],
+                                      amenities: data[index]['amenities']),
+                                );
+                              });
+                        }
+                      })
+                ],
+              ),
             ),
           ),
         ],
