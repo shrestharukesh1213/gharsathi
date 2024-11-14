f8099267c01dd3a9fe8cd6c7a868f24a9bf5db02
Rukesh Shrestha <125758326+shrestharukesh1213@users.noreply.github.com>
2024-11-14 21:08:19 +0545

    Changed TenantPreferenceScreen

--- lib/screens/TenantPreferenceScreen.dart
+++ lib/screens/TenantPreferenceScreen.dart
@@ -1,23 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:gharsathi/model/Preferences.dart';
 import 'package:gharsathi/services/PreferenceServices.dart';
 
-class TenantPreferenceScreen extends StatefulWidget {
-  const TenantPreferenceScreen({super.key});
+class Tenantpreferencescreen extends StatefulWidget {
+  const Tenantpreferencescreen({super.key});
 
   @override
-  State<TenantPreferenceScreen> createState() => _TenantPreferenceScreenState();
+  State<Tenantpreferencescreen> createState() => _TenantpreferencescreenState();
 }
 
-class _TenantPreferenceScreenState extends State<TenantPreferenceScreen> {
-  // Dropdown menu for location
+class _TenantpreferencescreenState extends State<Tenantpreferencescreen> {
+  //dropdownmenu for location
   String dropdownLocationValue = "Bhaktapur";
-  double _currentSliderValue = 20; // Distance slider value
-  String dropdownPropertyValue = "Apartment"; // Property type dropdown
-  RangeValues _currentRangeValues = const RangeValues(0, 100000); // Price range
+  // String? _location;
+  // final List<String> _location = [
+  //   'Bhaktapur',
+  //   'Kathmandu',
+  //   'Lalitpur',
+  //   'Kritipur'
+  // ];
 
-  // List of amenities and selection status
+  //location silder
+  double _currentSliderValue = 20;
+
+  //property Type
+  String dropdownPropertyValue = "Apartment";
+
+  //price range
+  RangeValues _currentRangeValues = const RangeValues(0, 100000);
+
+  // List of amenities with their current checked status
   final List<String> _amenitiesOptions = [
     'Gym',
     'School',
@@ -30,162 +43,118 @@ class _TenantPreferenceScreenState extends State<TenantPreferenceScreen> {
     'Banks',
     'Coffee Shops'
   ];
-  final List<bool> _isSelected = List.generate(10, (_) => false);
 
-  final PreferenceServices _preferenceServices = PreferenceServices();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadPreferences();
-  }
-
-  Future<void> _loadPreferences() async {
-    User? currentUser = FirebaseAuth.instance.currentUser;
-    if (currentUser != null) {
-      Preferences? preferences =
-          await _preferenceServices.getPreferences(currentUser.uid);
-
-      if (preferences != null) {
-        setState(() {
-          dropdownLocationValue = preferences.location;
-          _currentSliderValue = preferences.distance;
-
-          // Safely parse min and max values from priceRange
-          final minPrice = preferences.priceRange['min']?.toDouble() ?? 0.0;
-          final maxPrice =
-              preferences.priceRange['max']?.toDouble() ?? 100000.0;
-
-          _currentRangeValues = RangeValues(minPrice, maxPrice);
-          dropdownPropertyValue = preferences.propertyType;
-
-          for (int i = 0; i < _amenitiesOptions.length; i++) {
-            _isSelected[i] =
-                preferences.amenities.contains(_amenitiesOptions[i]);
-          }
-        });
-      }
-    }
-  }
+  final List<bool> _isSelected = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Preference Screen'),
+        title: Text('Preference Screen'),
         centerTitle: true,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          _buildDropdown('Location Preference', dropdownLocationValue,
-              ['Bhaktapur', 'Kathmandu', 'Lalitpur', 'Kritipur'], (value) {
-            setState(() {
-              dropdownLocationValue = value!;
-            });
-          }),
-          _buildSlider('Distance Preference (km)', _currentSliderValue,
-              (value) {
-            setState(() {
-              _currentSliderValue = value;
-            });
-          }),
-          _buildRangeSlider('Price Range', _currentRangeValues, (values) {
-            setState(() {
-              _currentRangeValues = values;
-            });
-          }),
-          _buildDropdown('Property Type', dropdownPropertyValue,
-              ['Apartment', 'House', 'Room'], (value) {
-            setState(() {
-              dropdownPropertyValue = value!;
-            });
-          }),
-          _buildAmenitiesChips(),
+          Text('Location Preference', style: TextStyle(fontSize: 18)),
+          DropdownButton(
+            hint: const Text('Select an option'),
+            items: const [
+              DropdownMenuItem(child: Text("Bhaktapur"), value: "Bhaktapur"),
+              DropdownMenuItem(child: Text("Kathmandu"), value: "Kathmandu"),
+              DropdownMenuItem(child: Text("Lalitpur"), value: "Lalitpur"),
+              DropdownMenuItem(child: Text("Kritipur"), value: "Kritipur"),
+            ],
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownLocationValue = newValue!;
+              });
+            },
+            value: dropdownLocationValue,
+          ),
+          Slider(
+            value: _currentSliderValue,
+            min: 0,
+            max: 100,
+            divisions: 10,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          Text('Price Range', style: TextStyle(fontSize: 18)),
+          RangeSlider(
+            values: _currentRangeValues,
+            min: 0,
+            max: 100000,
+            divisions: 20,
+            labels: RangeLabels(
+              _currentRangeValues.start.round().toString(),
+              _currentRangeValues.end.round().toString(),
+            ),
+            onChanged: (RangeValues values) {
+              setState(() {
+                _currentRangeValues = values;
+              });
+            },
+          ),
+          Text(
+            'Selected Range: ${_currentRangeValues.start.round()} - ${_currentRangeValues.end.round()}',
+            style: TextStyle(fontSize: 18),
+          ),
+          const SizedBox(height: 20),
+          Text('Property Type', style: TextStyle(fontSize: 18)),
+          DropdownButton(
+            hint: const Text('Select an option'),
+            items: const [
+              DropdownMenuItem(child: Text('Apartment'), value: 'Apartment'),
+              DropdownMenuItem(child: Text('House'), value: 'House'),
+              DropdownMenuItem(child: Text('Room'), value: 'Room'),
+            ],
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownPropertyValue = newValue!;
+              });
+            },
+            value: dropdownPropertyValue,
+          ),
+          const SizedBox(height: 20),
+          Text('Nearby Amenities', style: TextStyle(fontSize: 18)),
+          Wrap(
+            spacing: 8.0, // Spacing between chips
+            children:
+                List<Widget>.generate(_amenitiesOptions.length, (int index) {
+              return FilterChip(
+                label: Text(_amenitiesOptions[index]),
+                selected: _isSelected[index],
+                onSelected: (bool selected) {
+                  setState(() {
+                    _isSelected[index] = selected;
+                  });
+                },
+              );
+            }),
+          ),
           const SizedBox(height: 20),
           ElevatedButton(
-            onPressed: _savePreferencesToFirestore,
-            child: const Text('Save Preferences'),
-          ),
+              onPressed: _savePreferencesToFirestore,
+              child: const Text('Save Preferences')),
         ],
       ),
-    );
-  }
-
-  // Helper methods to build UI components
-  Widget _buildDropdown(String title, String value, List<String> items,
-      ValueChanged<String?> onChanged) {
-    return Column(
-      children: [
-        Text(title, style: const TextStyle(fontSize: 18)),
-        DropdownButton<String>(
-          value: value,
-          items: items
-              .map((item) => DropdownMenuItem(value: item, child: Text(item)))
-              .toList(),
-          onChanged: onChanged,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSlider(
-      String title, double value, ValueChanged<double> onChanged) {
-    return Column(
-      children: [
-        Text(title, style: const TextStyle(fontSize: 18)),
-        Slider(
-          value: value,
-          min: 0,
-          max: 100,
-          divisions: 10,
-          label: value.round().toString(),
-          onChanged: onChanged,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildRangeSlider(
-      String title, RangeValues values, ValueChanged<RangeValues> onChanged) {
-    return Column(
-      children: [
-        Text(title, style: const TextStyle(fontSize: 18)),
-        RangeSlider(
-          values: values,
-          min: 0,
-          max: 100000,
-          divisions: 20,
-          labels: RangeLabels(
-              values.start.round().toString(), values.end.round().toString()),
-          onChanged: onChanged,
-        ),
-        Text(
-          'Selected Range: ${values.start.round()} - ${values.end.round()}',
-          style: const TextStyle(fontSize: 16),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAmenitiesChips() {
-    return Column(
-      children: [
-        Text('Nearby Amenities', style: const TextStyle(fontSize: 18)),
-        Wrap(
-          spacing: 8.0,
-          children: List<Widget>.generate(_amenitiesOptions.length, (index) {
-            return FilterChip(
-              label: Text(_amenitiesOptions[index]),
-              selected: _isSelected[index],
-              onSelected: (bool selected) {
-                setState(() {
-                  _isSelected[index] = selected;
-                });
-              },
-            );
-          }),
-        ),
-      ],
     );
   }
 
@@ -193,15 +162,16 @@ class _TenantPreferenceScreenState extends State<TenantPreferenceScreen> {
     User? currentUser = FirebaseAuth.instance.currentUser;
 
     if (currentUser != null) {
-      List<String> selectedAmenities = _amenitiesOptions
-          .asMap()
-          .entries
-          .where((entry) => _isSelected[entry.key])
-          .map((entry) => entry.value)
-          .toList();
+      List<String> selectedAmenities = [];
+      for (int i = 0; i < _amenitiesOptions.length; i++) {
+        if (_isSelected[i]) {
+          selectedAmenities.add(_amenitiesOptions[i]);
+        }
+      }
 
+      // Create a Preferences object
       Preferences preferences = Preferences(
-        user: currentUser.displayName ?? '',
+        user: currentUser.displayName,
         uid: currentUser.uid,
         location: dropdownLocationValue,
         distance: _currentSliderValue,
@@ -213,10 +183,12 @@ class _TenantPreferenceScreenState extends State<TenantPreferenceScreen> {
         amenities: selectedAmenities,
       );
 
+      // Save the preferences using PreferenceServices
+      PreferenceServices preferenceServices = PreferenceServices();
       try {
-        await _preferenceServices.savePreferences(preferences);
+        await preferenceServices.savePreferences(preferences);
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Preferences saved successfully!')),
+          SnackBar(content: Text('Preferences saved successfully!')),
         );
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
